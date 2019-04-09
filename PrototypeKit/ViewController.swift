c47260c70e169effb5ea57ceae87b30339632775
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2019-04-09 13:57:42 +1000

    Failed attempt...

--- PrototypeKit/ViewController.swift
+++ PrototypeKit/ViewController.swift
@@ -6,13 +6,78 @@
 //  Copyright © 2018 nickTD. All rights reserved.
 //
 
+private class ShadowView: UIView {
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        func makeShadow() {
+            let context = UIGraphicsGetCurrentContext()!
+            let rectanglePath = UIBezierPath.init(
+                roundedRect: rect,
+                byRoundingCorners: [.topLeft, .topRight],
+                cornerRadii: CGSize(width: 16, height: 16)
+            )
+            rectanglePath.close()
+            context.saveGState()
+            context.setShadow(
+                offset: CGSize(width: 0, height: 5),
+                blur: 10,
+                color: UIColor.white.cgColor
+            )
+            UIColor.white.setFill()
+            rectanglePath.fill()
+            context.restoreGState()
+        }
+
+        makeShadow()
+    }
+}
+
 class ViewController: UIViewController {
+    private let contentView = ShadowView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .gray
+        self.view.backgroundColor = .gray
 
-        setUpLayers()
+//        setUpLayers()
+
+        contentView.backgroundColor = .white
+
+        view.addSubview(contentView)
+        contentView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalTo(100)
+        }
+
+//        addShadow(to: contentView)
+    }
+
+    private func addShadow(to view: UIView) {
+        view.layer.shadowColor = UIColor.white.cgColor
+        view.layer.shadowRadius = 10
+        view.layer.shadowOffset = CGSize(width: 0, height: -5)
+        view.layer.shadowOpacity = 1
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        let rect = self.contentView.bounds
+        let maskPath = UIBezierPath.init(
+            roundedRect: rect,
+            byRoundingCorners: [.topLeft, .topRight],
+            cornerRadii: CGSize(width: 16, height: 16)
+        )
+
+        let maskLayer = CAShapeLayer()
+
+        maskLayer.frame = rect
+        maskLayer.path = maskPath.cgPath
+
+        self.contentView.layer.mask = maskLayer
     }
 
     private func setUpLayers() {
