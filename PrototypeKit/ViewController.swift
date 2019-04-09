4e95f4ef1187da1be1f7f906d903b5b946375d7e
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2019-04-09 11:47:50 +1000

    Use layers for mask and shadow effects at the same time

--- PrototypeKit/ViewController.swift
+++ PrototypeKit/ViewController.swift
@@ -11,6 +11,51 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .gray
+
+        setUpLayers()
+    }
+
+    private func setUpLayers() {
+        func circleView(diameter: CGFloat) -> UIView {
+            let circle = UIView(frame:
+                CGRect(
+                    origin: .zero,
+                    size: CGSize(
+                        width: diameter,
+                        height: diameter
+                    )
+                )
+            )
+
+            circle.backgroundColor = .white
+            circle.layer.cornerRadius = diameter / 2
+            circle.layer.masksToBounds = true
+
+            return circle
+        }
+        func shadowLayer() -> CALayer {
+            let shadow = CALayer()
+
+            shadow.shadowColor = UIColor.white.cgColor
+            shadow.shadowRadius = 10
+            shadow.shadowOffset = CGSize(width: 0, height: 5)
+            shadow.shadowOpacity = 1
+
+            return shadow
+        }
+
+        let diameter: CGFloat = 100
+        let circle = circleView(diameter: diameter)
+
+        let shadow = shadowLayer()
+
+        circle.center = view.center
+//        view.addSubview(circle)
+
+        shadow.addSublayer(circle.layer)
+
+        view.layer.addSublayer(shadow)
+
     }
 }
 
